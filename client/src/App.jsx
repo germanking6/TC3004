d1c9f4fec0877c584456ff23478814ae89adb2b3
ariclavel <a01706629@itesm.mx>
2022-04-05 15:20:57 -0400

    app changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,50 @@
-import './App.css';
-import TableOutput from './TableOutput';
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import HeaderComponent from './components/HeaderComponent'
+
+
+
 function App() {
-  const titulos = ["id", "title","amount"];
-  const arreglo = [[1,"ariana",33],[2,"canela",5]];
+  const [count, setCount] = useState(0)
+
   return (
-    
     <div className="App">
-        <TableOutput contentRows= {arreglo} columnTitles = {titulos} />   
+      <HeaderComponent/>
+
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.jsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
     </div>
   )
 }
 
-export default App;
+export default App
