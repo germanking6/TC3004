6f83054340e2597ea9f2cf1983ee013cf8ca82ef
Marisol Melendres <marisolmelendresm@gmail.com>
2022-04-05 11:17:35 -0500

    Home page component

---
+++ client/src/pages/home-page/home-page.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './home-page.css';
+
+function HomePage() {
+    return (
+        <div className = 'homepage'>
+            <h1> WELCOME! </h1>
+            <h2> LERT - LABOR EXPENSES RECOVERY TOOL IS AN AID IN THE FINANCES OF THE MANAGER.
+                IT MANAGES SQUAD, EMPLOYEES AND CREATE THE FALL PLAN, PLANNING EXPENSES AND FINANCIAL RECOVERIES.
+            </h2>
+            <h3> THE GOAL OF THIS APPLICATIONS IS:</h3>
+            <p>-Reduction of invested by managers. <br/> -Having employee control.
+                <br/> -Ease of planning expense.
+                <br/>-Create financial recoveries.
+                <br/>-Create fall plan.
+            </p>
+        </div>
+    );
+}
+
+export default HomePage;
