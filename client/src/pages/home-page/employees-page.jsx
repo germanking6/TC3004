ecf4400800b9ddbe783674eba54b485dc9eebb7e
ariclavel <a01706629@itesm.mx>
2022-04-07 15:32:54 -0400

    add employee

--- client/src/pages/home-page/employees-page.jsx
+++ client/src/pages/home-page/employees-page.jsx
@@ -19,6 +19,7 @@ import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 const EmployeesPage = (props) =>{
     const a = [false, false,false,false,false,false,false,false,false,false,false,false];
     const [open, setOpen] = useState(a);
+    const [inputField, setInputField] = useState("");
     const columnTitles = ["Serial", "Country","Department","Department requester","Band","Type","Percentage recover","Date of start","Date of finish","ICA Manager","ICA","Squad"];
     const contentRows = [
         {
@@ -54,11 +55,40 @@ const EmployeesPage = (props) =>{
 
         }
     ];
-   /* const handleChange = (event) =>{
-        const {name, value} = event.target;
-        setFormFields({...formFields, [name]: value});
+
+    const [employees, setEmployees] = useState(contentRows);
+
+    const addEmployee = () =>{
+        console.log(employees);
+        console.log(contentRows);
+        console.log(inputField);
+        let obj = {
+            id: inputField,
+            Country: "",
+            EmployeeDepartment: "",
+            DepartmentRequester: "",
+            Band:2,
+            Type: 1,
+            PercentageRecover:50,
+            DateStart: "",
+            DateFinish:"",
+            ICAManager: "",
+            ica: 123,
+            Squad: "",
+            state: true
+        }
+        const arr = [...employees,obj];
+        setEmployees(arr);
+        setInputField("");
+        //setFormFields({...formFields, [name]: value});
+
         
-    };*/
+    };
+    const handleChange = (event) =>{
+        //console.log(event.target.value);
+        setInputField(event.target.value);
+
+    };
     const hidding = (event) =>{
         const temp = [...open];
         temp.splice(event.target.id, 1, !open[event.target.id]);
@@ -74,37 +104,13 @@ const EmployeesPage = (props) =>{
         
     );
 
-    const listItems = contentRows.map((item) =>
-        <TableRow key={item.id} >
-                <TableCell>{open[0] ? null : item.id}</TableCell>
-                <TableCell>{open[1] ? null : item.Country}</TableCell>
-                <TableCell>{open[2] ? null : item.EmployeeDepartment}</TableCell>
-                <TableCell>{open[3] ? null : item.DepartmentRequester}</TableCell>
-                <TableCell>{open[4] ? null : item.Band}</TableCell>
-                <TableCell>{open[5] ? null : item.Type}</TableCell>
-                <TableCell>{open[6] ? null : item.PercentageRecover}</TableCell>
-                <TableCell>{open[7] ? null : item.DateStart}</TableCell>
-                <TableCell>{open[8] ? null : item.DateFinish}</TableCell>
-                <TableCell>{open[9] ? null : item.ICAManager}</TableCell>
-                <TableCell>{open[10] ? null : item.ica}</TableCell>
-                <TableCell>{open[11] ? null : item.Squad}</TableCell>
-                <IconButton aria-label="delete" size="small" height="15" width="15">
-                    <DeleteIcon />
-                </IconButton>
-                <IconButton  size="small">
-                    <svg xmlns="http://www.w3.org/2000/svg" height="15" viewBox="0 0 24 24" width="15"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/></svg>
-                </IconButton>
-                <IconButton size="small">
-                    <svg xmlns="http://www.w3.org/2000/svg" height="15" viewBox="0 0 24 24" width="15"><path d="M0 0h24v24H0z" fill="none"/><path d="M13 3h-2v10h2V3zm4.83 2.17l-1.42 1.42C17.99 7.86 19 9.81 19 12c0 3.87-3.13 7-7 7s-7-3.13-7-7c0-2.19 1.01-4.14 2.58-5.42L6.17 5.17C4.23 6.82 3 9.26 3 12c0 4.97 4.03 9 9 9s9-4.03 9-9c0-2.74-1.23-5.18-3.17-6.83z"/></svg>
-                </IconButton>
-                
-        </TableRow>
-    );
+
+    
 
     return(
         <><form className='form' noValidate autoComplete="off">
-        <TextField id="outlined-basic" label="New Employee(s)" variant="outlined" />
-        <Button className='b' color="primary">Submit</Button>
+        <TextField id="outlined-basic" label="New Employee(s)" variant="outlined" onChange = {handleChange} />
+        <Button className='b' color="primary" onClick={addEmployee}>Submit</Button>
         <IconButton className='b' size="small">
             Import xsv
             <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 12v7H5v-7H3v7c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2v-7h-2zm-6 .67l2.59-2.58L17 11.5l-5 5-5-5 1.41-1.41L11 12.67V3h2z"/></svg>
@@ -117,8 +123,37 @@ const EmployeesPage = (props) =>{
                         {titleItems}    
                     </TableRow>
                 </TableHead>
-                <TableBody>
-                    {listItems}
+                <TableBody>    
+                    {    employees
+                        .map((item) =>(
+                        <TableRow key={item.id} >
+                        <TableCell>{open[0] ? null : item.id}</TableCell>
+                        <TableCell>{open[1] ? null : item.Country}</TableCell>
+                        <TableCell>{open[2] ? null : item.EmployeeDepartment}</TableCell>
+                        <TableCell>{open[3] ? null : item.DepartmentRequester}</TableCell>
+                        <TableCell>{open[4] ? null : item.Band}</TableCell>
+                        <TableCell>{open[5] ? null : item.Type}</TableCell>
+                        <TableCell>{open[6] ? null : item.PercentageRecover}</TableCell>
+                        <TableCell>{open[7] ? null : item.DateStart}</TableCell>
+                        <TableCell>{open[8] ? null : item.DateFinish}</TableCell>
+                        <TableCell>{open[9] ? null : item.ICAManager}</TableCell>
+                        <TableCell>{open[10] ? null : item.ica}</TableCell>
+                        <TableCell>{open[11] ? null : item.Squad}</TableCell>
+                        <IconButton aria-label="delete" size="small" height="15" width="15">
+                            <DeleteIcon />
+                        </IconButton>
+                        <IconButton  size="small">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="15" viewBox="0 0 24 24" width="15"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/></svg>
+                        </IconButton>
+                        <IconButton size="small">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="15" viewBox="0 0 24 24" width="15"><path d="M0 0h24v24H0z" fill="none"/><path d="M13 3h-2v10h2V3zm4.83 2.17l-1.42 1.42C17.99 7.86 19 9.81 19 12c0 3.87-3.13 7-7 7s-7-3.13-7-7c0-2.19 1.01-4.14 2.58-5.42L6.17 5.17C4.23 6.82 3 9.26 3 12c0 4.97 4.03 9 9 9s9-4.03 9-9c0-2.74-1.23-5.18-3.17-6.83z"/></svg>
+                        </IconButton>
+                 
+                    </TableRow>
+                    ))
+                    
+                }
+                    
                 </TableBody>
             </Table>
         </TableContainer></>
