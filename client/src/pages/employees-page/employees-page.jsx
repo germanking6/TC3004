c84f2bc2d355d867faca130f5ed2d2031982ad63
ariclavel <a01706629@itesm.mx>
2022-04-12 12:22:15 -0400

    comments

--- client/src/pages/employees-page/employees-page.jsx
+++ client/src/pages/employees-page/employees-page.jsx
@@ -27,8 +27,8 @@ import Box from '@mui/material/Box';
 //https://materialui.co/icon/expand-more
 
 const EmployeesPage = () =>{
-    const [inputField, setInputField] = useState("");
-     
+    //------TEMPORARY DATA FOR TESTING
+    //table data titles to simulate database
     const columnTitles = [
         {
             id: 0,
@@ -79,7 +79,7 @@ const EmployeesPage = () =>{
             title: "Squad"
         }
     ]
-    //const columnTitles = ["Serial", "Country","Department","Department requester","Band","Type","Percentage recover","Date of start","Date of finish","ICA Manager","ICA","Squad"];
+    //Employee data for simulate data base 
     const contentRows = [
         {
             id: "1234",
@@ -114,134 +114,42 @@ const EmployeesPage = () =>{
 
         }
     ];
+    //data of select bar for edit option (simulation and testing)
     const squads = [
     {
-    value: 'USD',
-    label: '$',
-  },
-  {
-    value: 'EUR',
-    label: '€',
-  },
-  {
-    value: 'BTC',
-    label: '฿',
-  },
-  {
-    value: 'JPY',
-    label: '¥',
-  },
+        value: 'USD',
+        label: '$',
+    },
+    {
+        value: 'EUR',
+        label: '€',
+    },
+    {
+        value: 'BTC',
+        label: '฿',
+    },
+    {
+        value: 'JPY',
+        label: '¥',
+    },
     ];
-    const [squad, setSquad] = useState('EUR');
-    //------EXCEL TABLE
-    const updateExcel = (e) =>{
-       console.log("import");
-       //https://stackoverflow.com/questions/62408085/how-to-get-the-data-from-excel-in-json-format-in-reactjs
-
-    }
-
-    //------COLUMNS
-    
-   //hide columns function
-   //variables
-    const a = [false, false,false,false,false,false,false,false,false,false,false,false];
-    const [open, setOpen] = useState(a);
-    //function
-    const hidding = (id, e) =>{
-        const temp = [...open];
-        temp.splice(id, 1, !open[id]);
-        setOpen(temp);
-    };
-    //sorting columns
-    //variables
-    const [sort,setSort] = useState(a);
-    //function
-    const sorting = (id) =>{
-        const keyObj = (Object.keys(employees[0])[id]);
-        const arr = [...employees];
-        console.log(keyObj);
-        //setEmployees(arr.sort(((a, b) => (Object.values(a)[id]) - Object.values(b)[id])));
-        setEmployees(arr.sort(((a, b) => ('' + Object.values(a)[id]).localeCompare(Object.values(b)[id]))));
-    }
 
     //------EMPLOYEES LIST USESTATE
     //modificating employees list
     const [employees, setEmployees] = useState(contentRows);
 
-    //------ALERTS AND ACTIONS ICON BUTTONS OF EACH ROW
 
-    //dialog alert variables setStatus
-    const [dialogDelete, setDialogDelete] = useState(false);
-    //IdDeleteEmployee
-    const [idDelete, setIdDelete] = useState();
-    //open dialog delete 
-    const openDialogDelete = (id) =>{
-        setDialogDelete(true);
-        setIdDelete(id);
-        //deleteEmployee(id);
-    }
-    
-    //function delete
-    const deleteEmployee = (id) =>{
-        setDialogDelete(true);
-        console.log(id);
-
-        let arreglo = [...employees];
-        console.log(arreglo.filter((item) => item.id != id));
-        setEmployees(arreglo.filter((item) => item.id != id));
-        console.log(employees);
-       
-    };
-    //for edit option
-    const [dialogEdit, setDialogEdit] = useState(false);
-    const [edit, setEdit] = useState(false);
-    const [editId, setEditId] = useState();
-    
-    const closeEdit = () => {
-        setEdit(false);
-        //setDialogEdit(true);
-        
-    };
-    //function edit
-    const editEmployee = (id) =>{
-        setEdit(true);
-        console.log("edit");
-    };
-    //for activate option
-    const [dialogActivate, setDialogActivate] = useState(false);
-    const [activateId, setActivateId] = useState();
-    const OpenActivateEmployee = (id) => {
-        setDialogActivate(true);
-        setActivateId(id);
-        
-    };
-    //function activate
-    const activateEmployee = (id) =>{
-        let arreglo = [...employees];
-        
-        arreglo.map((item) => {
-            if(item.id == id){
-                item.state = !item.state;
-            }
-            
-        });
-        setEmployees(arreglo);
-        
-    };
-    
-    //------ADD EMPLOYEES
-
-     //inputChange for adding employees        
-     const handleChange = (event) =>{
-        //console.log(event.target.value);
+    //------INPUT ADD EMPLOYEES
+    //Input State for adding employee
+    const [inputField, setInputField] = useState("");
+    //inputChange for adding employees        
+    const handleChange = (event) =>{
         setInputField(event.target.value);
 
     };
-  
+    //Function: adding employee 
     const addEmployee = () =>{
-        console.log(employees);
-        console.log(contentRows);
-        console.log(inputField);
+        //new employee object
         let obj = {
             id: inputField,
             Country: "",
@@ -257,14 +165,131 @@ const EmployeesPage = () =>{
             Squad: "",
             state: true
         }
+        //adding new employee to a temporary array
         const arr = [...employees,obj];
+        //adding employee to employee state list
         setEmployees(arr);
+        //setting input to empty
         setInputField("");   
        
     };
 
- 
-   
+
+    //------EXCEL TABLE
+    const updateExcel = (e) =>{
+       console.log("import");
+       //https://stackoverflow.com/questions/62408085/how-to-get-the-data-from-excel-in-json-format-in-reactjs
+
+    }
+
+    //------COLUMNS
+   //hide columns function
+   //variables
+    //setting false to all columns
+    const a = [false, false,false,false,false,false,false,false,false,false,false,false];
+    //Hidding state
+    const [open, setOpen] = useState(a);
+    //Hidding function
+    const hidding = (id, e) =>{
+        //temporary array
+        const temp = [...open];
+        //edit variable for open/hide columns in temporary array
+        temp.splice(id, 1, !open[id]);
+        //edit state 
+        setOpen(temp);
+    };
+    //sorting columns
+    //variables
+    const [sort,setSort] = useState(a);
+    //function
+    const sorting = (id) =>{
+        //temporary array for employees
+        const arr = [...employees];
+        //sorting employees depending on id column for sorting task
+        setEmployees(arr.sort(((a, b) => ('' + Object.values(a)[id]).localeCompare(Object.values(b)[id]))));
+         //temporary array
+         const temp = [...sort];
+         //edit variable for open/hide columns in temporary array
+         temp.splice(id, 1, !sort[id]);
+         //edit sort state 
+         setSort(temp);
+    }
+
+
+
+    //------ALERTS AND ACTIONS ICON BUTTONS OF EACH ROW
+
+    //------DELETE EMPLOYEES
+    //VARIABLES
+    //dialog alert variables setStatus
+    const [dialogDelete, setDialogDelete] = useState(false);
+    //IdDeleteEmployee
+    const [idDelete, setIdDelete] = useState();
+
+    //opendialog delete 
+    const openDialogDelete = (id) =>{
+        setDialogDelete(true);
+        setIdDelete(id);
+    }
+    
+    //function delete
+    const deleteEmployee = (id) =>{
+        //temporary array
+        let arreglo = [...employees];
+        //filter/delete element
+        setEmployees(arreglo.filter((item) => item.id != id));
+       
+    };
+
+    //------EDIT EMPLOYEES
+    //VARIABLES
+    //temporary data for testing
+    const [squad, setSquad] = useState('EUR');
+    //open dialog edit
+    const [dialogEdit, setDialogEdit] = useState(false);
+    //edit option or cancel task
+    const [edit, setEdit] = useState(false);
+    //id for delete option
+    const [editId, setEditId] = useState();
+    
+    //closing edit window of inputs
+    const closeEdit = () => {
+        setEdit(false); 
+    };
+    //function edit
+    const editEmployee = (id) =>{
+        setEdit(true);
+        console.log("edit");
+    };
+
+    //------ACTIVATE/DESACTIVATE EMPLOYEES
+    //VARIABLES
+    //open dialog activate/desactivate
+    const [dialogActivate, setDialogActivate] = useState(false);
+    //idStatus
+    const [activateId, setActivateId] = useState();
+    //openDialog
+    const OpenActivateEmployee = (id) => {
+        setDialogActivate(true);
+        setActivateId(id);      
+    };
+    //function activate
+    const activateEmployee = (id) =>{
+        //temporary array
+        let arreglo = [...employees];
+        //mapping array to change status of element id
+        arreglo.map((item) => {
+            if(item.id == id){
+                item.state = !item.state;
+            }
+            
+        });
+        //change state of employees
+        setEmployees(arreglo);  
+    };
+    
+
+    //---------COLUMNS TITLES + BUTTON TO HIDE AND SORT ELEMENTS
     const titleItems = columnTitles.map((item) =>
         <TableCell key={item.id}>
             {item.title}
@@ -274,6 +299,7 @@ const EmployeesPage = () =>{
         </TableCell> 
         
     );
+    //---------HTML
     return(
    
             <TableContainer className='table'>
@@ -288,7 +314,6 @@ const EmployeesPage = () =>{
              </form>
                 <Table sx={{ minWidth: 30}} aria-label="simple table">
                     <TableHead>
-                    
                         <TableRow>
                             
                             {titleItems}    
@@ -318,9 +343,6 @@ const EmployeesPage = () =>{
                                     </IconButton>
                                     <Alert elementId ={idDelete} toDo= {deleteEmployee} dialog={dialogDelete} setDialog={setDialogDelete} ide= {item.id} message= {"Delete employee"} />
                                     
-                                      
-                                    
-                                    
                                     <IconButton  size="small"  id={item.id} onClick={() => editEmployee(item.id)}>
                                         <svg xmlns="http://www.w3.org/2000/svg" height="15" viewBox="0 0 24 24" width="15"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" id={item.id} /></svg>
                                     </IconButton>
@@ -331,7 +353,6 @@ const EmployeesPage = () =>{
                                         <svg xmlns="http://www.w3.org/2000/svg" height="15" viewBox="0 0 24 24" width="15"><path d="M0 0h24v24H0z" fill="none"/><path d="M13 3h-2v10h2V3zm4.83 2.17l-1.42 1.42C17.99 7.86 19 9.81 19 12c0 3.87-3.13 7-7 7s-7-3.13-7-7c0-2.19 1.01-4.14 2.58-5.42L6.17 5.17C4.23 6.82 3 9.26 3 12c0 4.97 4.03 9 9 9s9-4.03 9-9c0-2.74-1.23-5.18-3.17-6.83z" id={item.id} /></svg>
                                     </IconButton>
                                     <Alert elementId ={activateId} dialog={dialogActivate} setDialog={setDialogActivate} toDo={activateEmployee} ide= {item.id} message= {"Change employee state (activat/desactivate)"} />
-
                                     
                                 </TableCell>   
                                          
