89c4f99760c1cac97073a2fe31f4fc1d5c037b69
cheveuxdelin <cheveuxdelin@icloud.com>
2022-04-18 21:51:34 -0500

    component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,14 @@ import './App.css'
 import HeaderComponent from './components/HeaderComponent'
 import SideBar from './components/sidebar'
 
+import Reports from "./pages/Reports";
+
 
 function App() {
   const [count, setCount] = useState(0)
   return (
     <div className="App">
+      <Reports/>
     </div>
   )
 }
