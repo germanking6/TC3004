17e004e65619da578a599e1ae07587294e4e63ed
Rodrigo Saucedo <rodrisc2000@hotmail.com>
2022-04-18 19:47:27 -0500

    Correct to return the App component
    Return App component instead of EmployeesPage

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,15 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-//import {EmployeesPage} from "./pages\home-page/employees-page";
 import CssBaseline from '@mui/material/CssBaseline';
-import EmployeesPage from './pages/employees-page/employees-page';
-
 
 ReactDOM.render(
   <React.StrictMode>
     <CssBaseline/>
-    <EmployeesPage />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 )
