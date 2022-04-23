6695322deb6df1841f93277264899d482fa3c3b7
Wiki-10 <alexhdzcontacto@gmail.com>
2022-04-23 15:28:45 -0500

    Refactoring the login into smaller components

--- client/src/components/Login/InputForm.jsx
+++ client/src/components/Login/InputForm.jsx
@@ -1,79 +1,86 @@
-import { Box, flexbox } from "@mui/system";
-import Grid from "@mui/material/Grid";
+import * as React from "react";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import userJson from "../../assets/data.json";
 import {
-  Paper,
-  CssBaseline,
-  Typography,
   Avatar,
-  FormControlLabel,
-  TextField,
-  Checkbox,
   Button,
-  Link,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Paper,
+  Box,
+  Grid,
+  Typography,
 } from "@mui/material";
+import { useState } from "react";
+import InputFooter from "./InputFooter";
 
-const InputForm = (props) => {
-  const { sx, ...other } = props;
+export default function InputForm() {
+  const [sucessfulLogin, setSucessfulLogin] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    let email = data.get("email");
+    let password = data.get("password");
+
+    const userData = userJson.find((user) => user.email === email);
+    console.log(userData);
+  };
+
   return (
-    <Grid container component="main">
-      <CssBaseline />
-      <Grid />
-      <Grid component={Paper} elevation={6} square>
-        <div>
-          <Avatar>
-            <p>a</p>
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <form noValidate>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button type="submit" fullWidth variant="contained" color="primary">
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-            <Box mt={5}></Box>
-          </form>
-        </div>
-      </Grid>
+    <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+      <Box
+        sx={{
+          my: 8,
+          mx: 4,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+          <InputFooter />
+        </Box>
+      </Box>
     </Grid>
   );
-};
-
-export default InputForm;
+}
