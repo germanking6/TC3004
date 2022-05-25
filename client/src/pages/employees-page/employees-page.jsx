8c7a3bcead7b842aaf06af5771b8c69862a8b12f
ariclavel <a01706629@itesm.mx>
2022-05-25 13:50:01 -0400

    error correccion

--- client/src/pages/employees-page/employees-page.jsx
+++ client/src/pages/employees-page/employees-page.jsx
@@ -25,7 +25,7 @@ import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import * as XLSX from "xlsx";
-import { Autocomplete } from '@mui/material';
+import { Autocomplete, Container } from '@mui/material';
 
 
 
@@ -522,7 +522,7 @@ const EmployeesPage = () =>{
                                     <IconButton aria-label="delete" size="small" height="15" width="15" key={item.id} id={item.id} onClick={() => openDialogDelete(item.id)}>
                                         <DeleteIcon />
                                     </IconButton>
-                                    <Alert elementId ={idDelete} toDo= {deleteEmployee} dialog={dialogDelete} setDialog={setDialogDelete} ide= {item.id} message= {"Delete employee"} />
+                                    <Container><Alert elementId ={idDelete} toDo= {deleteEmployee} dialog={dialogDelete} setDialog={setDialogDelete} ide= {item.id} message= {"Delete employee"} /></Container>
                                     
                                     <IconButton  size="small"  id={item.id} onClick={() => editEmployee(item.id)}>
                                         <svg xmlns="http://www.w3.org/2000/svg" height="15" viewBox="0 0 24 24" width="15"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" id={item.id} /></svg>
