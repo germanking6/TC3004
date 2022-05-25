da3ec919374bf2a8692defddd56e62c756faad53
ariclavel <a01706629@itesm.mx>
2022-05-25 08:39:48 -0400

    correccion

---
+++ client/src/components/test/employeeTest.jsx
@@ -0,0 +1,8 @@
+//employee.test.js
+
+import { configure, shallow, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import renderer from 'react-test-renderer';
+import React from 'react';
+import { employee } from '../pages/employees-page/employees-page';
+
