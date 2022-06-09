971c2a43a0c1b6b49fca179bd744400d309f152c
SauceRSC <rodrisc2000@hotmail.com>
2022-06-09 14:44:14 -0500

    Create countries name and value json

---
+++ client/src/pages/employees-page/countries.jsx
@@ -0,0 +1,74 @@
+export const countries = [
+  {
+    value: "Australia",
+    label: "Australia",
+  },
+  {
+    value: "Belgium",
+    label: "Belgium",
+  },
+  {
+    value: "Bolivia (Plurinational State of)",
+    label: "Bolivia (Plurinational State of)",
+  },
+  {
+    value: "Canada",
+    label: "Canada",
+  },
+  {
+    value: "Cayman Islands",
+    label: "Cayman Islands",
+  },
+  {
+    value: "Chile",
+    label: "Chile",
+  },
+  {
+    value: "Christmas Island",
+    label: "Christmas Island",
+  },
+  {
+    value: "Colombia",
+    label: "Colombia",
+  },
+  {
+    value: "Mexico",
+    label: "Mexico",
+  },
+  {
+    value: "Monaco",
+    label: "Monaco",
+  },
+  {
+    value: "Netherlands",
+    label: "Netherlands",
+  },
+  {
+    value: "Saudi Arabia",
+    label: "Saudi Arabia",
+  },
+  {
+    value: "Singapore",
+    label: "Singapore",
+  },
+  {
+    value: "Spain",
+    label: "Spain",
+  },
+  {
+    value: "United Arab Emirates",
+    label: "United Arab Emirates",
+  },
+  {
+    value: "United Kingdom of Great Britain and Northern Ireland",
+    label: "United Kingdom of Great Britain and Northern Ireland",
+  },
+  {
+    value: "United States of America",
+    label: "United States of America",
+  },
+  {
+    value: "Venezuela (Bolivarian Republic of)",
+    label: "Venezuela (Bolivarian Republic of)",
+  },
+];
